5b079bbbb8de606cd0234ec7d2fdcd1e4fdb9e34
benwzj <57896457+benwzj@users.noreply.github.com>
2021-01-14 11:00:47 +1100

    Initial Commit

---
+++ MachineLearnigWithApple/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearnigWithApple
+//
+//  Created by Ben Wen on 14/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
